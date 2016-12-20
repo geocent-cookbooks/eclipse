ea8f71b66522096306423fbf87cd63a7175d94f2
Christopher Williams <chris.a.williams@gmail.com>
2016-12-20 15:15:52 -0500

    Use checksum for eclipse if in attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,13 +3,13 @@
 # Recipe:: default
 #
 # Copyright (C) 2013 Geocent, LLC
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,6 +31,7 @@ end
 ark "eclipse" do
   url eclipse_url
   version node['eclipse']['version']
+  checksum node['eclipse']['checksum'] if node['eclipse']['checksum']
   extension "tar.gz"
   has_binaries ['eclipse']
   append_env_path true
